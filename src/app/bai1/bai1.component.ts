11e0b30cb327ac3fe64455b93379470b58052183
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-05-25 13:02:55 +0700

    "Added CSS styles for Bai1 and Bai2 components, and implemented Bai1 component with HTML, TypeScript, and Bootstrap modal for cart functionality."

--- src/app/bai1/bai1.component.ts
+++ src/app/bai1/bai1.component.ts
@@ -11,5 +11,52 @@ import {RouterLink, RouterOutlet} from "@angular/router";
   styleUrls: ['./bai1.component.css']
 })
 export class Bai1Component {
+  products: any[] = [
+    {
+      id: 1, title: 'Macbook Pro', price: 25000000, qty: 1, image:
+        './assets/images/1.jpg'
+    },
+    {
+      id: 2, title: 'Asus ROG Gaming', price: 17000000, qty: 1, image:
+        './assets/images/2.jpg'
+    },
+    {
+      id: 3, title: 'Amazon Kindle', price: 15000000, qty: 1, image:
+        './assets/images/3.jpg'
+    },
+    {
+      id: 4, title: 'Another Product', price: 18000000, qty: 1, image:
+        './assets/images/4.jpg'
+    },
+  ]
+  cartItems: any[] = []
 
+  addToCart(item: any) {
+// Add the item or increase qty
+    let itemInCart = this.cartItems.filter(item => item.id === item.id);
+    let isItemInCart = itemInCart.length > 0;
+    if (!isItemInCart) {
+      this.cartItems.push({
+        id: item.id, title: item.title,
+        price: item.price, qtys: item.qty, image: item.image
+      })
+    } else {
+      itemInCart[0].qtys += item.qty;
+    }
+    item.qty = 1;
+    console.log(this.cartItems)
+    console.log(this.products)
+  }
+
+  removeItem(index: any) {
+    this.cartItems.splice(index, 1);
+  }
+  Total(){
+    let sum = 0;
+    this.cartItems.forEach(item => sum += item.qtys * item.price)
+  }
+  totalItems(){
+    let sum = 0;
+    this.cartItems.forEach(item => sum += item.qtys)
+  }
 }
